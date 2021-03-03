80046c9f1596f614242c776856daaf2be061bc45
Aris Pratama <arispratama15@gmail.com>
2021-03-03 23:07:54 +0700

    add more complex selector DOM

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
-const myHead = document.getElementById('myHeading');
+const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
+const revertBlack = document.getElementById('revertBLack');
+const paragraph = document.querySelector('.paragraph');
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
 
 myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
-}
-);
+    if (myHeading.style.color == myTextInput.value){
+        myButton.textContent = 'change color';
+        myHeading.style.color = 'black';
+    }else {
+        myButton.textContent = 'revert to black';
+        myHeading.style.color = myTextInput.value;
+    }
+});
+
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show list';                        
+    listDiv.style.display = 'none';
+  }                         
+});
+
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ':';
+});
